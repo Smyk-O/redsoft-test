8fb315698b8f9e7cf1db78c4170e50f36ed2eef0
Smyk <smyk006@yandex.ru>
2020-09-01 01:11:04 +0300

    Updates

---
+++ precache-manifest.c947ab62a5d327027bfa1c8586b300c6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c7df02ef4ba79c86f2fbf3dd199d92",
+    "url": "/redsoft-test/index.html"
+  },
+  {
+    "revision": "0f594df5f3a7ecf219ff",
+    "url": "/redsoft-test/static/css/main.c3a4c07c.chunk.css"
+  },
+  {
+    "revision": "ef072eb45fd96efaca34",
+    "url": "/redsoft-test/static/js/2.9b824de2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redsoft-test/static/js/2.9b824de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f594df5f3a7ecf219ff",
+    "url": "/redsoft-test/static/js/main.264cf224.chunk.js"
+  },
+  {
+    "revision": "199fc4f48c9c947b3e85",
+    "url": "/redsoft-test/static/js/runtime-main.8f65854a.js"
+  },
+  {
+    "revision": "8ac2b15ffe9c53dd89754349dad9edfc",
+    "url": "/redsoft-test/static/media/picture_1.8ac2b15f.png"
+  },
+  {
+    "revision": "9d66729387ac2fedadcbdf382fe98131",
+    "url": "/redsoft-test/static/media/picture_2.9d667293.png"
+  },
+  {
+    "revision": "5f219440d9944558a958d1e3d6509bcb",
+    "url": "/redsoft-test/static/media/picture_3.5f219440.png"
+  },
+  {
+    "revision": "775a18a9c708af91a7233230926533d8",
+    "url": "/redsoft-test/static/media/picture_4.775a18a9.png"
+  }
+]);
